0d08e2749abc2f6cd67af02da5a0f1fe222bf62f
ClaytonCampbell <ccampbell9764@gmail.com>
2020-09-28 16:58:17 -0400

    Split into classes

--- Program.cs
+++ Program.cs
@@ -7,45 +7,49 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            int lives = 5;
-            string[] HangmanAnswers = { "boogie", "taco", "extreme" };
-            Random newAnswer = new Random();
-            var tko = newAnswer.Next(0, 3);
-            string hangmanAnswer = HangmanAnswers[tko];
+            Start _difficulty = new Start();
+            int lives = _difficulty.Select();
+            WordGenerate _wg = new WordGenerate();
+            string hangmanAnswer = _wg.NewWord();
             char[] answerArray = new char [hangmanAnswer.Length];
             Console.WriteLine($"Please guess a letter in the word. There are {answerArray.Length} letters in it.");
 
             for (int i = 0; i < hangmanAnswer.Length; i++)
-                answerArray[i] = '_';
-            while (true)
+                answerArray[i] = '#';
+                var g = true;
             {
-                char hangmanGuess = Console.ReadKey().KeyChar;
-                for (int l = 0; l < hangmanAnswer.Length; l++)
+                while (g)
                 {
-                    if (hangmanGuess == hangmanAnswer[l])
-                        answerArray[l] = hangmanGuess;
-                }
-            if (Array.IndexOf(answerArray, hangmanGuess) < 0)
-            {
-                lives -= 1;
-                if (lives == 0)
+                    char hangmanGuess = Console.ReadKey().KeyChar;
+                    for (int l = 0; l < hangmanAnswer.Length; l++)
+                    {
+                        if (hangmanGuess == hangmanAnswer[l])
+                            answerArray[l] = hangmanGuess;
+                    }
+                    if (Array.IndexOf(answerArray, hangmanGuess) < 0)
+                    {
+                        lives -= 1;
+                        if (lives == 0)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You've lost all your parts. \nGame Over.");
+                            Console.ReadLine();
+                            g = false;
+                        }
+                    }
+                    Console.WriteLine("\n");
+                    Console.WriteLine(answerArray);
+                    Console.WriteLine(lives);
+                    string result = new string(answerArray);
+                    if (result == hangmanAnswer)
                     {
                         Console.Clear();
-                        Console.WriteLine("You've lost all your parts. \nGame Over.");
+                        Console.WriteLine("You've won the game!");
                         Console.ReadLine();
+                        g = false;
                     }
-            }
-                Console.WriteLine("\n");
-                Console.WriteLine(answerArray);
-                Console.WriteLine(lives);
-                string result = new string(answerArray);
-            if (result == hangmanAnswer)
-                {
-                    Console.Clear();
-                    Console.WriteLine("You've won the game!");
                 }
             }
-            Console.ReadLine();
         }
     }
 }
---
+++ WordGenerate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman
+{
+    public class WordGenerate
+    {
+        string[] HangmanAnswers = { "boogie", "taco", "extreme" };
+        public string NewWord()
+        {
+            Random newAnswer = new Random();
+            var tko = newAnswer.Next(0, 3);
+            string hangmanAnswer = HangmanAnswers[tko];
+            return hangmanAnswer;
+        }
+    }
+}
